abc9234f9cbf7a2af5a5d136ec4c027479ce0fbd
seers777 <107905314+seers777@users.noreply.github.com>
2022-11-24 23:08:33 +0800

    Update Cubox.js

--- .github/Cubox.js
+++ .github/Cubox.js
@@ -16,32 +16,4 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/cubox.js
-
-[mitm] 
-
-hostname = cubox.pro
-
-*******************************/
-
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const vip = '/userInfo';
-
-
-if (url.indexOf(vip) != -1) {
-    obj.data.level = 1;
-    obj.data.expireTime = "2099-09-12T23:50:23+08:00";
-    obj.data.nickName = "彭于晏解锁";
-    obj.data.thirdNickName = "彭于晏解锁";
-    obj.data.isExpire = false;
-    obj.data.active = true;
-    obj.data.payTime = 1660006006;
-
-	body = JSON.stringify(obj);
-}
-
-
-$done({body});
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://github.com/seers777/Surge/raw/master/.github/Cubox.js
