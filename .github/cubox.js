545c707d8c66cf56ee7f37250fce12e8a7fabde3
seers777 <107905314+seers777@users.noreply.github.com>
2022-11-24 23:15:06 +0800

    Update cubox.js

--- .github/cubox.js
+++ .github/cubox.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/cubox.js
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://github.com/seers777/Surge/raw/master/.github/cubox.js
 
 [mitm] 
 
