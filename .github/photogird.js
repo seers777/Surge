e353d2ffbc3ad0c3af23f464047eaeaae41936ec
seers777 <107905314+seers777@users.noreply.github.com>
2023-08-30 17:32:56 +0800

    Create photogird.js

---
+++ .github/photogird.js
@@ -0,0 +1,14 @@
+/*
+
+[rewrite_local]
+^https://pgapi.ksmobile.com/v1/ios/auth url script-response-body https://github.com/seers777/Surge/raw/master/.github/photogird.js
+
+[MITM]
+hostname = pgapi.ksmobile.com
+
+*/
+var guding = $response.body;
+guding = guding.replace(/"expires_date":\d+/g, '"expires_date":148204937166');
+guding  = guding.replace(/"is_trial_period":\w+/g, '"is_trial_period":false');
+guding = guding.replace(/"expires_date_ms":\d+/g, '"expires_date_ms":148204937166000');
+$done({ body:guding});
