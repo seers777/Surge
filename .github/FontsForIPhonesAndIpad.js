5006113f82314ac4490a3cb6dc9a93cee0113320
seers777 <107905314+seers777@users.noreply.github.com>
2023-08-30 20:11:38 +0800

    Update FontsForIPhonesAndIpad.js

--- .github/FontsForIPhonesAndIpad.js
+++ .github/FontsForIPhonesAndIpad.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/fonts.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://github.com/seers777/Surge/raw/master/.githu/FontsForIPhonesAndIpad.js
 
 [mitm]
 hostname = buy.itunes.apple.com
