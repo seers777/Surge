4263ce81121b7dde26c00410c749d3e1481c944e
seers777 <107905314+seers777@users.noreply.github.com>
2023-09-22 10:32:30 +0800

    Update Yase.js

--- .github/Yase.js
+++ .github/Yase.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yase.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://github.com/seers777/Surge/blob/master/.github/Yase.js
 
 
 [MITM]
